94e04d7595185c181b0aa5b8cec129fc17679b23
Ismaguel <moussaismaguel01@gmail.com>
2023-09-25 15:10:25 +0100

    Add ChartJS

--- Lib.Web/Controllers/HomeController.cs
+++ Lib.Web/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using Lib.Web.Models;
+﻿using Lib.Web.Data;
+using Lib.Web.Models;
+using Lib.Web.ViewModel;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -7,15 +9,26 @@ namespace Lib.Web.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly Context _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, Context context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var totalBooks = _context.Books.Count();
+            var totalImprinters = _context.Imprinters.Count();
+          //var totalBookImprinters = _context.BookImprinters.Count();
+
+            var viewModel = new DashboardViewModel
+            {
+                TotalBooks = totalBooks,
+                TotalImprinters = totalImprinters
+            };
+            return View(viewModel);
         }
 
         public IActionResult Privacy()
---
+++ Lib.Web/ViewModel/DashboardViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Lib.Web.Models;
+using System;
+
+namespace Lib.Web.ViewModel
+{
+    public class DashboardViewModel
+    {
+        public int TotalBooks { get; set; }
+        public int TotalImprinters { get; set; }
+    }
+}
